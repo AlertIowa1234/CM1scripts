b0d456e3bc893d7fe6bd6b70d8abd4088c17c6a4
AlertIowa1234 <16069928+AlertIowa1234@users.noreply.github.com>
2019-02-27 18:29:37 +0000

    Update CM1toSHARPpy.py

--- CM1scripts/CM1toSHARPpy.py
+++ CM1scripts/CM1toSHARPpy.py
@@ -10,7 +10,7 @@ def usage(name=None):
    return '''CM1toSHARPpy.py input-netCDF-file output-sounding-file [options] [--help]
 
 Example:
-  python3 CM1toSHARPpy.py cm1out.nc CM1SHARP.out -x 0 -y 0 -t 52 -u 12.4 -v 1.9 -windinterp 1
+  python3 CM1toSHARPpy.py cm1out.nc CM1SHARP.out -x 0 -y 0 -t 52 -u 12.0 -v 8.0
 '''
 
 #Define command line arguments
@@ -22,7 +22,6 @@ parser.add_argument('-y', help='y position to generate sounding data from datase
 parser.add_argument('-t', help='time to generate sounding data from in dataset', type=float, default=0.)
 parser.add_argument('-u', help='X movement speed of domain in m/s', type=float, default=0.)
 parser.add_argument('-v', help='Y movement speed of domain in m/s', type=float, default=0.)
-parser.add_argument('-windinterp', help='Use wind vectors interpolated to scalar points?', type=int, default=0)
 args = parser.parse_args()
 datafile = netCDF4.Dataset(args.inputfile, 'r')
 
@@ -32,24 +31,16 @@ TIME = datafile.variables['time'][:]
 TH   = datafile.variables['th'][args.t,:,args.y,args.x] * units.degK
 QV   = datafile.variables['qv'][args.t,:,args.y,args.x] * units('g/kg')
 TMPK = mpcalc.temperature_from_potential_temperature(PRS, TH)
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Wind direction calculation currently uses non interpolated wind vectors interpolated to scalar points, use the flag -windinterp 1 to use the interpolated variables.
-if args.windinterp == 0:
- U    = np.array(datafile.variables['u'][args.t,:,args.y,args.x]) + args.u
- V    = np.array(datafile.variables['v'][args.t,:,args.y,args.x]) + args.v
-elif args.windinterp == 1:
- U    = np.array(datafile.variables['uinterp'][args.t,:,args.y,args.x]) + args.u
- V    = np.array(datafile.variables['vinterp'][args.t,:,args.y,args.x]) + args.v
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+U    = np.array(datafile.variables['uinterp'][args.t,:,args.y,args.x]) + args.u
+V    = np.array(datafile.variables['vinterp'][args.t,:,args.y,args.x]) + args.v
+
 #Define output variables.
-#------------------------------------------------------------------------------------------
-#Still have not found a way to use zf or zh from model data because of numpy giving errors.
-HGHT = datafile.variables['z'][:] * 1000.00 * units.meters
-#------------------------------------------------------------------------------------------
+HGHT = datafile.variables['zh'][args.t,:,args.y,args.x] * 1.00 * units.meters
 TMPC = TMPK.to('degC')
 DWPC = mpcalc.dewpoint(mpcalc.vapor_pressure(PRS, QV))
 WDIR = mpcalc.wind_direction(U, V)
 WSPD = mpcalc.wind_speed(U, V) * 1.94384
+
 # Write data to file.
 file_header = """%TITLE%
 Xaxis_{0}_Yaxis_{1}_Time_{2}   010101/0000
